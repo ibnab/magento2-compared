3c715ebf42d22e2d95434ac06c7a9c25043f8c97
said elmarrouani <contact@ibnab.com>
2015-09-29 23:54:16 +0100

    the new commit

---
+++ Controller/Adminhtml/Index/Compare.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Ibnab\ComparedProduct\Controller\Adminhtml\Index;
+
+class Compare extends \Magento\Customer\Controller\Adminhtml\Index
+{
+    /**
+     * Customer compare grid
+     *
+     * @return \Magento\Framework\View\Result\Layout
+     */
+    public function execute()
+    {
+        $this->initCurrentCustomer();
+        $resultLayout = $this->resultLayoutFactory->create();
+        return $resultLayout;
+    }
+}
